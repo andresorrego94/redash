af5a7b0841846c9db82919823bf232ec4a2a06ec
Mathias <Lantean>
2021-11-07 19:07:05 -0300

    Add support for using workers on MacOS

--- redash/tasks/worker.py
+++ redash/tasks/worker.py
@@ -3,11 +3,21 @@ import os
 import signal
 import time
 from redash import statsd_client
+from sys import platform
 from rq import Queue as BaseQueue, get_current_job
+from rq import Worker as BaseWorker
 from rq.worker import HerokuWorker # HerokuWorker implements graceful shutdown on SIGTERM
 from rq.utils import utcnow
 from rq.timeouts import UnixSignalDeathPenalty, HorseMonitorTimeoutException
 from rq.job import Job as BaseJob, JobStatus
+
+
+# HerokuWorker does not work in OSX https://github.com/getredash/redash/issues/5413
+def get_worker_base_class():
+    if platform == "darwin":
+        return BaseWorker
+    else:
+        return HerokuWorker
 
 
 class CancellableJob(BaseJob):
@@ -41,7 +51,7 @@ class RedashQueue(StatsdRecordingQueue, CancellableQueue):
     pass
 
 
-class StatsdRecordingWorker(HerokuWorker):
+class StatsdRecordingWorker(get_worker_base_class()):
     """
     RQ Worker Mixin that overrides `execute_job` to increment/modify metrics via Statsd
     """
@@ -59,7 +69,7 @@ class StatsdRecordingWorker(HerokuWorker):
                 statsd_client.incr("rq.jobs.failed.{}".format(queue.name))
 
 
-class HardLimitingWorker(HerokuWorker):
+class HardLimitingWorker(get_worker_base_class()):
     """
     RQ's work horses enforce time limits by setting a timed alarm and stopping jobs
     when they reach their time limits. However, the work horse may be entirely blocked
