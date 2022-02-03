9550530f6e7b1ed086d7d3dc046fa23a8e05102e
Andres Orrego <andres.orrego.94@gmail.com>
2022-02-03 05:22:30 -0300

    Pointing to firebase

--- client/app/pages/flow/Flow.jsx
+++ client/app/pages/flow/Flow.jsx
@@ -7,8 +7,8 @@ const Flow = () => {
   return (
     <>
       <div style={{ height: '100%' }}>
-        {/* <iframe style={{ width: '100%', height: '100%' }} src="https://tablyx-flow-designer.web.app/" title="W3Schools Free Online Web Tutorials"></iframe> */}
-        <iframe style={{ width: '100%', height: '100%' }}  src="http://localhost:3000/token-prueba" title="Flow Engine Design"></iframe>
+        <iframe style={{ width: '100%', height: '100%' }} src="https://tablyx-flow-designer.web.app/" title="W3Schools Free Online Web Tutorials"></iframe>
+        {/* <iframe style={{ width: '100%', height: '100%' }}  src="http://localhost:3000/token-prueba" title="Flow Engine Design"></iframe> */}
         {/* <object style={{ width: '100%', height: '100%' }} type="text/html" data="https://tablyx-flow-designer.web.app/"></object> */}
       </div>
     </>
